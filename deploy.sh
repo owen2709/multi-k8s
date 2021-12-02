32915b7d2b3138d22b54cfdfde21377b2f897dac
Owen Micallef <owen.micallef.19@um.edu.mt>
2021-12-02 16:07:56 +0100

    added deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrinder/multi-client-k8s:latest -t stephengrinder/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrinder/multi-server-k8s-pgfix:latest -t stephengrinder/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrinder/multi-worker-k8s:latest -t stephengrinder/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t owen2709/multi-client-k8s:latest -t owen2709/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t owen2709/multi-server-k8s-pgfix:latest -t owen2709/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t owen2709/multi-worker-k8s:latest -t owen2709/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrinder/multi-client-k8s:latest
-docker push stephengrinder/multi-server-k8s-pgfix:latest
-docker push stephengrinder/multi-worker-k8s:latest
+docker push owen2709/multi-client-k8s:latest
+docker push owen2709/multi-server-k8s-pgfix:latest
+docker push owen2709/multi-worker-k8s:latest
 
-docker push stephengrinder/multi-client-k8s:$SHA
-docker push stephengrinder/multi-server-k8s-pgfix:$SHA
-docker push stephengrinder/multi-worker-k8s:$SHA
+docker push owen2709/multi-client-k8s:$SHA
+docker push owen2709/multi-server-k8s-pgfix:$SHA
+docker push owen2709/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrinder/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=stephengrinder/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrinder/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=owen2709/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=owen2709/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=owen2709/multi-worker-k8s:$SHA
